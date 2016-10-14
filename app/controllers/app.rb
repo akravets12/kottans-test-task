2f9f67adedb675235b2d9b97eead257bb196700b
Igor Onischenko <optimum.flaminis@gmail.com>
2016-10-15 00:06:44 +0300

    static files fixed

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
-
+require_relative '../models/message.rb'
 class SafeMessagingApp < Sinatra::Base
   set :haml, :format => :html5
   set :views, File.join(Dir.pwd, 'app', 'views')
+  set :public_folder, Proc.new { File.join(Dir.pwd, "public") }
   get '/' do
-    haml :index
+    haml :index, locals: { root: settings.root }
+  end
+
+  post '/' do
+    flash = { message: "ok" }
+    haml :index, locals: flash
   end
 end
